84ca8ccd9fe7a48045c3e208e23b04ca20ae8aa2
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-02-09 18:37:00 +0100

    bootstrap + index: tutti i risultati

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,9 +1,28 @@
 @extends('layouts.base')
 
 @section('pageContent')
-    <ul>
-        @foreach ($comics as $comic)
-            <li>{{ $comic->title }}</li>
-        @endforeach
-    </ul>
+  <h1 class="text-center my-4 text-uppercase font-weight-bold">Tutti i fumetti</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Tipo</th>
+                <th scope="col">Prezzo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row">{{ $comic->id }}</th>
+                    <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->series }}</td>
+                    <td>{{ $comic->type }}</td>
+                    <td>$ {{ $comic->price }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
