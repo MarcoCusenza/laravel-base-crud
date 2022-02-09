f583e68e9f5fdb9014c2033f2a008fbc579849ba
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-02-09 17:34:59 +0100

    resourceController - route - index

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.base')
+
+@section('pageContent')
+    <ul>
+        @foreach ($comics as $comic)
+            <li>{{ $comic->title }}</li>
+        @endforeach
+    </ul>
+@endsection
